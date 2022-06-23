92250c1fb19168e7a9dc1532be06d988611adc96
lizhenzhen <969411602@qq.com>
2022-06-23 23:05:43 +0800

    fix: 百度推送token

--- baiduPush.sh
+++ baiduPush.sh
@@ -4,6 +4,6 @@
 set -e
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://lizhenzhen.xyz&token=T5PEAzhGaPNbjQ2X"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://blog.lizhenzhen.xyz&token=sToyjopBdijibFzt"
 
 rm -rf urls.txt # 删除文件
